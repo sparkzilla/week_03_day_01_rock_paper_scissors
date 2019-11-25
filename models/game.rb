57254eaf9163034f7783354da21a0c7de6292432
user <user@users-MacBook-Pro-8.local>
2019-11-25 22:48:25 +0000

    Add game logic

---
+++ models/game.rb
@@ -0,0 +1,28 @@
+class Game
+
+  def self.do(act1, act2)
+    if (act1 == act2)
+      return "It's a draw"
+    else
+      case act1 == "rock"
+        when act2 == "paper"
+          return "Paper beats rock"
+        when act2 = "scissors"
+          return "Rock beats scissors"
+      end
+      case act1 == "paper"
+        when act2 == "scissors"
+         return "Scissors beats paper"
+        when act = "rock"
+        return "Paper beats scissors"
+      end
+      case act1 == "scissors"
+      when act2 == "paper"
+         return "Scissors beats paper"
+        when act = "rock"
+        return "Rock beats scissors"
+      end
+    end
+  end
+
+end
