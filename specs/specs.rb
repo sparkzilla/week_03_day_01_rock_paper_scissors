0d49cfb5200e4acbfc0008794c8a187e3a1aaa0e
user <user@users-MacBook-Pro-8.local>
2019-11-25 22:37:35 +0000

    Add rg

--- specs/specs.rb
+++ specs/specs.rb
@@ -1,9 +1,11 @@
 require 'minitest/autorun'
+require 'minitest/rg'
+
 require_relative '../models/game'
 
 class TestGame < Minitest::Test
 
-  def test_win
+  def test_draw
     assert_equal( "It's a draw", Game.do("rock", "rock") )
   end
 
